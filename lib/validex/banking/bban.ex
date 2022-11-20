07b64df0a6086b56940ba88e2e14482ed7de1acf
eltonplima <me@eltonplima.dev>
2022-11-20 22:20:08 +0000

    Fix broken test

--- lib/validex/banking/bban.ex
+++ lib/validex/banking/bban.ex
@@ -6,10 +6,10 @@ defmodule Validex.Banking.BBAN do
 
   @doc """
   iex> Validex.Banking.BBAN.new("000788565563853695532")
-  %Validex.Banking.BBAN{bank_identifier: "0007", local_account_number: "88565563853695532"}
+  %Validex.Banking.BBAN{bank_identifier: "0007", local_account_number: "88565563853695532", normalized: "000788565563853695532"}
 
   iex> Validex.Banking.BBAN.new("000788565563853695532")
-  %Validex.Banking.BBAN{bank_identifier: "0007", local_account_number: "88565563853695532"}
+  %Validex.Banking.BBAN{bank_identifier: "0007", local_account_number: "88565563853695532", normalized: "000788565563853695532"}
   """
   def new(bban) when is_binary(bban) do
     regex = ~r/^(?P<bank_identifier>\d{4})(?P<local_account_number>\d{17})/
