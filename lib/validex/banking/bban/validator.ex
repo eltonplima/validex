70310437a100a3bcc4a4ba2296a20fa6963f1ed4
eltonplima <me@eltonplima.dev>
2022-11-20 20:41:51 +0000

    Update BBAN.Validator
     - Updated valid?/1 to accept BBAN structure

--- lib/validex/banking/bban/validator.ex
+++ lib/validex/banking/bban/validator.ex
@@ -19,10 +19,23 @@ defmodule Validex.Banking.BBAN.Validator do
   iex> alias Validex.Banking.BBAN.Validator
   iex> Validator.valid?(853114809472329760320)
   false
+
+  iex> alias Validex.Banking.BBAN.Validator
+  iex> alias Validex.Banking.BBAN
+  iex> Validator.valid?(%BBAN{bank_identifier: "8531", local_account_number: "14809472329760320", normalized: "853114809472329760320"})
+  false
+
+  iex> alias Validex.Banking.BBAN.Validator
+  iex> alias Validex.Banking.BBAN
+  iex> Validator.valid?(%BBAN{bank_identifier: "8531", local_account_number: "14809472329760329", normalized: "853114809472329760329"})
+  true
   """
 
   # TODO: Validate length
-  # TODO: Accept BBAN structure
+  def valid?(%BBAN{} = bban) do
+    valid?(bban.normalized)
+  end
+
   def valid?(bban) when is_binary(bban) do
     {bban, _} = Integer.parse(bban)
     valid?(bban)
